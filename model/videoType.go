d2b337aaed8a14da332d393d0621a5c052645117
isBoom <1520285660@qq.com>
2021-01-16 14:55:08 +0800

    limit offset

--- model/videoType.go
+++ model/videoType.go
@@ -1,7 +1,8 @@
 package model
 
-var VideoTypes  = [...]string {"anime","movie","funny","other"}
+var VideoTypes = [...]string{"anime", "movie", "funny", "other"}
+
 type VideoType struct {
-	ID        uint `gorm:"primary_key"`
-	VideoType string `json:"videoType" gorm:"unique varchar(20)"`
+	ID        uint   `gorm:"primary_key"`
+	VideoType string `json:"videoType" gorm:"unique;size:20;not null"`
 }
--- service/list_video_service.go
+++ service/list_video_service.go
@@ -10,20 +10,26 @@ type ListVideoServics struct {
 	Title     string `json:"title" form:"title"`
 	Info      string `json:"info" form:"info"`
 	VideoType string `json:"videoType" form:"videoType"`
+	Limit     uint64 `json:"limit" form:"limit"`
+	Offset    uint64 `json:"offset" form:"offset"`
 }
 
 func (s *ListVideoServics) List() serializer.Response {
 	videos := []model.Video{}
-	where:=[]string{}
+	where := []string{}
 	var count uint64
-	if s.Info!=""{
-		where = append(where," info like '%"+s.Info+"%' ")
+	if s.Info != "" {
+		where = append(where, " info like '%"+s.Info+"%' ")
 	}
-	if s.Title!=""{
-		where = append(where," title like '%"+s.Title+"%' ")
+	if s.Title != "" {
+		where = append(where, " title like '%"+s.Title+"%' ")
 	}
+	if s.Limit == 0 {
+		s.Limit = 12
+	}
+
 	if err := model.DB.Where(&model.Video{
-		VideoType:s.VideoType,
+		VideoType: s.VideoType,
 	}).Where(strings.Join(where, "or")).
 		Find(&videos).Count(&count).Error; err != nil {
 		return serializer.Response{
@@ -32,8 +38,19 @@ func (s *ListVideoServics) List() serializer.Response {
 			Error: err.Error(),
 		}
 	}
+
+	if err := model.DB.Where(&model.Video{
+		VideoType: s.VideoType,
+	}).Where(strings.Join(where, "or")).Offset(s.Offset).Limit(s.Limit).
+		Find(&videos).Error; err != nil {
+		return serializer.Response{
+			Code:  5001,
+			Msg:   "查询视频数据失败",
+			Error: err.Error(),
+		}
+	}
 	return serializer.Response{
-		Data: serializer.BuildVideos(videos),
-		Count:count,
+		Data:  serializer.BuildVideos(videos),
+		Count: count,
 	}
 }
